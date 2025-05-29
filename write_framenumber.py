6a72defdf755ce194f595c3a76a2d6c47fbcfb14
altarterminal <118420278+nm-jp@users.noreply.github.com>
2025-05-29 23:28:22 +0900

    update

--- write_framenumber.py
+++ write_framenumber.py
@@ -30,13 +30,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('in_file', type=str)
 parser.add_argument('-p', '--points', type=str, default='')
 parser.add_argument('-d', '--out-dir', type=str, default='', help='output direcotry')
-parser.add_argument('-r', '--is-only-round', action='store_true')
+parser.add_argument('--is-round-only', action='store_true')
 
 args = parser.parse_args()
 in_file = args.in_file
 points  = args.points
 out_dir = args.out_dir
-is_only_round = args.is_only_round
+is_round_only = args.is_round_only
 
 if not os.access(in_file, os.F_OK) or not os.access(in_file, os.R_OK):
   output_error('invalid file specified <' + in_file + '>')
@@ -156,7 +156,7 @@ while True:
     print(out_file, flush=True)
 
     frame_number += 1
-    if (frame_number == point_num) and is_only_round:
+    if is_round_only and (frame_number == point_num):
       break
 
 #####################################################################
